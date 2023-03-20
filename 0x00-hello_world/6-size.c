246c16de90f7ae5b1fad2d102782afd96573389d
NAJO199 <elbaroudinajlae@gmail.com>
2023-03-20 14:28:25 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,11 @@
 */
 int main(void)
 {
-
 char c;
 int i;
 long li;
 long long lli;
 float f;
-
 printf("size of a char: %ld byte(s)\n", sizeof(c));
 printf("size of a int: %ld byte(s)\n", sizeof(i));
 printf("size of a long: %ld byte(s)\n", sizeof(li));
